70d6ca60375caad5093a2be006bf1f64b4d1073a
doubletb00 <doubletb@gmail.com>
2021-05-04 11:00:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,29 @@
 variable "quote_id" {
+  type        = "string"
+  description = "The quote ID for this order."
   default = ""
 }
 
 variable "hostname" {
+  type        = "string"
+  description = "The hostname for this server."
   default = ""
 }
 
 variable "domain" {
+  type        = "string"
+  description = "The domain name for this server"
   default = "cadence.poc"
 }
 
 variable "datacenter" {
+  type        = "string"
+  description = "The data center to deploy this servere in."
   default = "sjc04"
 }
 
 variable "hourly_billing" {
+  type        = bool
+  description = "Is this server billed hourly - true/false."
   default = true
 }
