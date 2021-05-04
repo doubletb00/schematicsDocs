cb91aaa3297d2b9d7a4cbdbbc14b18f88267edbd
doubletb00 <doubletb@gmail.com>
2021-05-04 10:45:19 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "service_name" {
+  default = "myservice"
+}
+
+variable "service_type" {
+  default = "cloud-object-storage"
+}
+
+variable "plan" {
+  default = "lite"
+}
+
+variable "location" {
+  default = "global"
+}
+
+variable "name" {
+  default = "default"
+}
