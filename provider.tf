09021029182ffbb04dac54d929eff86cf05e09eb
doubletb00 <doubletb@gmail.com>
2021-05-04 10:49:22 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+variable "iaas_classic_username" {
+   type = "string"
+}
+variable "iaas_classic_api_key" {
+   type = "string"
+}
+
+provider "ibm" {
+  region = "sjc04"
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
+}
