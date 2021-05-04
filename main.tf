e512104dc1278d85b105272d8d12b345116c1b4a
doubletb00 <doubletb@gmail.com>
2021-05-04 11:40:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "ibm_compute_vm_instance" "vm1" {
   hostname             = var.hostname
   domain               = var.domain
   quote_id             = var.quote_id
+  datacenter           = var.datacenter
 
   # Optional fields
-  # datacenter           = var.datacenter
   # network_speed        = 1000
   # hourly_billing       = var.hourly_billing
   # private_network_only = true
